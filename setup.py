f85237d81c0dc8e9113dfee69640b2c13e8cb820
Bineeth <40259631+bineeth-weavedin@users.noreply.github.com>
2018-08-13 15:49:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ except ImportError:
 
 setup(
     name='Eve',
-    version='0.6.4',
+    version='0.6.4a',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Nicola Iarocci',
